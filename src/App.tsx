327a8f20404207e0a71e0ffb60fac0d93853ba61
denvudd <dimontv7@gmail.com>
2024-04-24 16:13:29 +0300

    init: project setup

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import HomePage from "./pages/HomePage";
+import NotFoundPage from "./pages/NotFoundPage";
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/NotFoundPage.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import NotFound from "../components/common/NotFound";
+
+interface NotFoundPageProps {}
+
+const NotFoundPage: React.FC<NotFoundPageProps> = ({}) => {
+  return <NotFound />;
+};
+
+export default NotFoundPage;
